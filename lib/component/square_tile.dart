8f732f519da43e755c176e564c939e1c69650eda
IbrahimAzab <54166348+simplehima@users.noreply.github.com>
2024-05-30 03:22:04 +0300

    Added square_tile.dart

---
+++ lib/component/square_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  final String imagePath;
+  final Function()? onTap;
+  const SquareTile({
+    super.key,
+    required this.imagePath,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        decoration: const BoxDecoration(
+
+
+
+        ),
+        child: Image.asset(
+          imagePath,
+          height: 80,
+        ),
+      ),
+    );
+  }
+}
